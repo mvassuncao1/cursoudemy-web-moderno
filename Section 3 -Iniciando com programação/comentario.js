344320708dd2ecafb7e09882db9167393c62d31b
Marcos Assunção <mvassuncao1@gmail.com>
2026-03-07 20:48:55 -0300

    aula comentarios

---
+++ Section 3 -Iniciando com programação/comentario.js
@@ -0,0 +1,13 @@
+// comentário de uma linha
+
+/* comentário
+ de múltiplas
+ linhas */
+
+/* usar
+ * asterisco para
+ * organizar melhor
+ * o comentário
+ * de múltiplas
+ * linhas */
+
---
+++ Section 3 -Iniciando com programação/sentenca_2.js
@@ -0,0 +1,8 @@
+console.log(1);
+console.log(1 + 1);
+console.log("aspas simples");
+console.log("aspas duplas");
+console.log("aspas duplas com 'aspas simples' dentro");
+console.log('aspas simples com "aspas duplas" dentro');
+console.log("aspas duplas com \"aspas duplas\" dentro");
+console.log('aspas simples com \'aspas simples\' dentro');
