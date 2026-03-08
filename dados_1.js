c51623c69d14181a6f15634a972ecf2531372cad
Marcos Assunção <mvassuncao1@gmail.com>
2026-03-07 21:29:03 -0300

    assisti a aula 1 de dados

---
+++ dados_1.js
@@ -0,0 +1,14 @@
+console.log("Caneta"); //
+console.log(10);       //
+console.log(6.4);      //
+console.log(true);    //4
+
+var a = 3; //variável do tipo number
+let b = 4.5; //variável do tipo number
+const c = "Olá"; //variável do tipo string
+let d = true; //variável do tipo boolean
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
