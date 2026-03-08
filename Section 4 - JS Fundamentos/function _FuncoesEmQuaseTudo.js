2e84769591a6df3e20069274c28b607934cb8cbe
Marcos Assunção <mvassuncao1@gmail.com>
2026-03-08 16:17:29 -0300

    Terminei primeira aula de funções e dei uma pausa

---
+++ Section 4 - JS Fundamentos/function _FuncoesEmQuaseTudo.js
@@ -0,0 +1,6 @@
+console.log(typeof Object) // Object é uma função, ou seja, é um tipo de função.
+class Produto{}
+console.log(typeof Produto) // Produto é uma função, ou seja, é um tipo de função.
+
+// Em JavaScript, quase tudo é uma função, incluindo objetos, classes e até mesmo funções em si. Isso ocorre porque as funções são tratadas como cidadãos de primeira classe na linguagem, o que significa que podem ser atribuídas a variáveis, passadas como argumentos para outras funções e retornadas como valores de outras funções. Portanto, é comum encontrar muitos tipos de dados em JavaScript que são, na verdade, funções.
+
