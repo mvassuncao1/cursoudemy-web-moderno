8c06ae055397cb01d8933444285691f682bec7f6
Marcos Assunção <mvassuncao1@gmail.com>
2026-03-10 09:37:37 -0300

    finalizei a aula 62 > Notação Ponto

--- Section 4 - JS Fundamentos/notacaoPonto.js
+++ Section 4 - JS Fundamentos/notacaoPonto.js
@@ -1,14 +1,17 @@
 const obj1 = {}; // Criando um objeto vazio
 console.log(typeof obj1); // tipo do obj1 é 'object'
-obj1.nome = "Marcos";
+obj1.nome = "Marcos"; // notação literal para adicionar um atributo 'nome' ao objeto obj1
 obj1["idade"] = 42;
 console.log(obj1);
 
-function funcionario(nomeEnviado) {
+function funcionario(nomeEnviado) { // ultiliza Function Constructor para criar um objeto do tipo funcionario
   this.nome = nomeEnviado;
+  this.informaNome = function () {
+    console.log("O nome do funcionário é", this.nome);
+  };
 }
 
 func1 = new funcionario("Marcos");
 console.log(func1.nome); // Imprime "Marcos"
-
+func1.informaNome(); // Imprime "O nome do funcionário é Marcos" usando a função informaNome do objeto func1
 
