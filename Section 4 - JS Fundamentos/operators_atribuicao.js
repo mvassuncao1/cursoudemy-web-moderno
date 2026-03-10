fc6cc92f693f8fa85a7cbaf1213bd85e30f99e75
Marcos Assunção <mvassuncao1@gmail.com>
2026-03-10 10:40:48 -0300

    Estudei aula 63 > operador de atribuição e suas variações

---
+++ Section 4 - JS Fundamentos/operators_atribuicao.js
@@ -0,0 +1,18 @@
+const a = 7
+let   b = 4
+
+b += a // b = b + a // atribuição aditiva
+console.log(b) // O resultado é 11, pois o valor de b (4) é somado ao valor de a (7) e o resultado é atribuído novamente a b.
+
+b -= 4 // b = b - 4 // atribuição subtrativa
+console.log(b) // O resultado é 7, pois o valor atual de b (11) é subtraído por 4 e o resultado é atribuído novamente a b.
+
+b *= 2 // b = b * 2 // atribuição multiplicativa 
+console.log(b) // O resultado é 14, pois o valor atual de b (7) é multiplicado por 2 e o resultado é atribuído novamente a b.
+
+b /= 2 // b = b / 2 // atribuição divisiva
+console.log(b) // O resultado é 7, pois o valor atual de b (14) é dividido por 2 e o resultado é atribuído novamente a b.
+
+b %= 2 // b = b % 2 // atribuição modular
+console.log(b) // O resultado é 1, pois o valor atual de b (7) é dividido por 2 e o resto da divisão é atribuído novamente a b.
+
