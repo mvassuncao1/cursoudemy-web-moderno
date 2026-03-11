b5bf34d30b228568df19d8e18d2ff98aafb9a1ca
Marcos Assunção <mvassuncao1@gmail.com>
2026-03-11 10:10:58 -0300

    Finalzei aula 70. Operadores Lógicos

--- Section 4 - JS Fundamentos/operator_logic.js
+++ Section 4 - JS Fundamentos/operator_logic.js
@@ -13,12 +13,32 @@
 function xor(a, b) {
   return (a || b) && !(a && b);
 }
-console.log("V xor V ->", xor(true,  true));  // false
-console.log("V xor F ->", xor(true,  false)); // true
-console.log("F xor V ->", xor(false, true));  // true
+console.log("V xor V ->", xor(true, true)); // false
+console.log("V xor F ->", xor(true, false)); // true
+console.log("F xor V ->", xor(false, true)); // true
 console.log("F xor F ->", xor(false, false)); // false
 
 // 70. Operadores: Lógicos => 4:09
 // Criei a branch develop no repositório para testes
 // Commitei alterações na branch develop
 // Fiz merge da branch develop para a main
+
+//-------------------------------------------------------
+/* Eu tenho dois trabalhos para fazer
+    - Se fizer os dois trabalhos eu compro uma TV 55"
+    - Se fizer um trabalho apenas eu compro uma TV 32"
+    - Se fizer um dos dois ou os dois eu tomo sorvete
+    - Se não comprar sorvete eu fico saudável
+*/
+function compras(trabalho1, trabalho2) {
+  const comprarSorvete = trabalho1 || trabalho2;
+  const comprarTV55 = trabalho1 && trabalho2;
+  //const comprarTV32 = trabalho1 ^ trabalho2; // operador bitwise XOR para simular o comportamento do 'xor'
+  const comprarTV32 = trabalho1 != trabalho2;  // outra forma de simular o 'xor' usando desigualdade
+  const saudavel = !comprarSorvete;
+  console.log(comprarSorvete, comprarTV55, comprarTV32, saudavel);
+}
+compras(true,  true);  // true  true  false false
+compras(true,  false); // true  false true  false
+compras(false, true);  // true  false true  false
+compras(false, false); // false false false true
