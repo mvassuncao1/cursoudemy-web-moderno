43b755e94a3a98ed5cdd5841352922ca4c494665
Marcos Assunção <mvassuncao1@gmail.com>
2026-03-11 07:32:51 -0300

    Ajustei formatação do arquivo operator_logic.js

--- Section 4 - JS Fundamentos/operator_logic.js
+++ Section 4 - JS Fundamentos/operator_logic.js
@@ -2,19 +2,20 @@
 
 // O 'xor' retorna true se apenas um dos operandos for verdadeiro, e false caso contrário. Podemos usar a seguinte expressão para simular o 'xor':
 
-/* OU EXCLUSIVO XOR:
-        V xor V -> F
-        V xor F -> V
-        F xor V -> V
-        F xor F -> F
+/* 
+  OU EXCLUSIVO XOR:
+      V xor V -> F
+      V xor F -> V
+      F xor V -> V
+      F xor F -> F
 */
 
 function xor(a, b) {
   return (a || b) && !(a && b);
 }
-console.log("V xor V ->)", xor(true, true)); // false
-console.log("V xor F ->)", xor(true, false)); // true
-console.log("F xor V ->)", xor(false, true)); // true
-console.log("F xor F ->)", xor(false, false)); // false
+console.log("V xor V ->", xor(true,  true));  // false
+console.log("V xor F ->", xor(true,  false)); // true
+console.log("F xor V ->", xor(false, true));  // true
+console.log("F xor F ->", xor(false, false)); // false
 
 // 70. Operadores: Lógicos => 4:09
