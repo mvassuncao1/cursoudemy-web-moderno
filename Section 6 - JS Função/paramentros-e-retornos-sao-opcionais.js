1605d8569a14095e1ec37e43ae78a3b09cc573a2
Marcos  Vitor Alves Starling Assuncao marcosassuncao <marcosassuncao@pbh.gov.br>
2026-03-24 08:28:02 -0300

    Estudo de Funções

---
+++ Section 6 - JS Função/paramentros-e-retornos-sao-opcionais.js
@@ -0,0 +1,9 @@
+function area(largura, altura) {
+  const area = largura * altura;
+  if (area > 20) return area;
+  else return area;
+}
+
+console.log(area(1, 200));
+console.log(area(10));
+console.log(area(1, 200, 3));
