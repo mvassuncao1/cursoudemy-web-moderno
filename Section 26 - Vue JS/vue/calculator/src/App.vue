ccec55b1db2837fe3d196986195b99fd10eb87a1
Marcos  Vitor Alves Starling Assunção <mvassuncao1@gmail.com>
2026-04-09 11:11:47 -0300

    Iniciado projeto Calculadora Finalizada aula 489

--- Section 26 - Vue JS/vue/calculator/src/App.vue
+++ Section 26 - Vue JS/vue/calculator/src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>Olá, Vue</h1>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+export default{
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    @font-face {
+        font-family: "RobotoMono";
+        src: url("./fonts/RobotoMono-Thin.ttf");
+    }
+
+    * {
+        font-family: "RobotoMono", monospace;
+    }
+
+    body {
+        margin: 0;
+    }
+
+    #app {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+
+        color: #fff;
+        background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+    }
 </style>
--- Section 26 - Vue JS/vue/calculator/src/main.js
+++ Section 26 - Vue JS/vue/calculator/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import App from './App'
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  //el: '#app', no arquivo de exemplo do Vue esse passo é executado com o método .$mount('#app') do objeto Vue
+  render: createElement => createElement(App) 
+//render(createElement) { return createElement(App)},; //no arquivo de exemplo do Vue essa função é criada apenas com a arrow function acima 
+}).$mount('#app');
