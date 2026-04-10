b6d9a30d3fe3503bef3044a01448702dd837b7a9
Marcos  Vitor Alves Starling Assunção <mvassuncao1@gmail.com>
2026-04-10 16:06:41 -0300

    Fim de experiente: finalizada Aula 494 mas calculadora não se comporta como esperado. Assistir aula novamente e validar código

--- Section 26 - Vue JS/vue/calculator/src/main/AppCalculator.vue
+++ Section 26 - Vue JS/vue/calculator/src/main/AppCalculator.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="calculator">
-        <Display value="1000"/>
+        <Display :value="displayValue"/>
         <Button label="AC" triple @onCalcButtonClick="clearMemory"/>
         <Button label="/" operation @onCalcButtonClick="setOperation"/>
         <Button label="8" @onCalcButtonClick="addDigit"/>
@@ -26,16 +26,61 @@ import Display from '../components/CalculatorDisplay.vue';
 import Button from '../components/CalculatorButton.vue';
 
 export default{
+    data: function() {
+        return {
+            displayValue: "0",
+            clearDisplay: false,
+            operation: null,
+            values: [0, 0],
+            current: 0
+        }
+    },
     components: {Button, Display},
     methods:{
         clearMemory(){
-            console.log("Limpar memória")
+            Object.assign(this.$data, this.$options.data())
         },
         setOperation(operation){
-            console.log("operation", operation)
+            if(this.current === 0){
+                this.operation = operation
+                this.current = 1
+                this.clearDisplay = true
+            } else {
+                const equals = operation === "="
+                const currentOperation = this.operation
+
+                try{
+                    this.value = eval(
+                        `${this.values[0]} ${currentOperation} ${this.values[1]}`
+                    )
+                } catch(e){
+                    this.$emit('onError', e)
+                }
+
+                this.values[1] = 0
+
+                this.displayValue = this.values[0]
+                this.operation = equals ? null : operation
+                this.current = equals ? 0 : 1
+                this.clearDisplay = !equals
+            }
         },
         addDigit(n){
-            console.log("Digit", n)
+            if (n === "." && this.displayValue.includes(".")){
+                return
+            }
+            const clearDisplay = this.displayValue === "0" || this.clearDisplay
+            const currentValue = clearDisplay ? "" : this.displayValue
+            const displayValue = currentValue + n
+
+            this.displayValue = displayValue
+            this.clearDisplay = false
+
+            if(n !== "."){
+                const i = this.current
+                const newValue = parseFloat(displayValue)
+                this.values[i] = newValue
+            }
         }
     }
 }
