9931f682d60b424e977fdfb6474e59449ca8c1cf
Marcos  Vitor Alves Starling Assunção <mvassuncao1@gmail.com>
2026-04-10 14:34:22 -0300

    Finalizada aula 490 Iniciada a escrita do Calculator.vue. Aula interrompida para participar de Daily

--- Section 26 - Vue JS/vue/calculator/src/App.vue
+++ Section 26 - Vue JS/vue/calculator/src/App.vue
@@ -1,12 +1,14 @@
 <template>
     <div id="app">
-        <h1>Olá, Vue</h1>
+        <h1>Calculator</h1>
+        <Calculator />
     </div>
 </template>
 
 <script>
+import Calculator from './main/AppCalculator.vue';
 export default{
-
+    components: {Calculator}
 }
 </script>
 
